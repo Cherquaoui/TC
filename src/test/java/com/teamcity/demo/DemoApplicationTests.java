81107a74df6d9d8123b57e6baa4e969868d52e8c
Cherquaoui <cherq.habib@gmail.com>
2019-10-23 21:38:46 +0100

    first commit

---
+++ src/test/java/com/teamcity/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
